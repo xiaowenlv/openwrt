235ee5fa2b50cf99965e0d1d105cefb43f56d3cb
xiaowenlv <38743488+xiaowenlv@users.noreply.github.com>
2025-11-10 19:19:11 +0800

    Enhance security and update network settings in script
    Updated the script to clear the default password more securely and modify network interface configurations.

--- diy-part2.sh
+++ diy-part2.sh
@@ -5,13 +5,22 @@
 # This is free software, licensed under the MIT License.
 # See /LICENSE for more information.
 #
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-#
 
-# 1. 修改默认IP地址为 192.168.99.3
+# 1. 修改默认IP地址为 192.168.99.3 (这个方法是标准的，保留)
 sed -i 's/192.168.1.1/192.168.99.3/g' package/base-files/files/bin/config_generate
 
-# 2. 清除默认密码 (密码为空)
-sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
+# 2. 清除默认密码 (更安全的方式)
+#    查找包含 "root:" 的行，并将后面的密码哈希部分替换为空
+sed -i 's/root:[^:]*/root:/' package/base-files/files/etc/shadow
+
+# 3. 更改网络接口配置 (更安全的方式)
+#    只修改 device 选项，不破坏文件结构
+echo "Modifying network config..."
+# 将 LAN 口的 device 修改为 eth1
+sed -i "/config interface 'lan'/,/option proto 'static'/{s/option device '[^']*'/option device 'eth1'/}" package/base-files/files/etc/config/network
+# 将 WAN 口的 device 修改为 eth0
+sed -i "/config interface 'wan'/,/option proto 'dhcp'/{s/option device '[^']*'/option device 'eth0'/}" package/base-files/files/etc/config/network
+# 将 WAN6 口的 device 修改为 eth0
+sed -i "/config interface 'wan6'/,/option proto 'dhcpv6'/{s/option device '[^']*'/option device 'eth0'/}" package/base-files/files/etc/config/network
+
+echo "LAN IP set to 192.168.99.3, password cleared, eth0(WAN)/eth1(LAN) configured."
